3f087a5485ec356e2cec5d25a9e9091a13c416ac
artturalvaro <artturalvaro@gmail.com>
2025-06-14 17:08:45 -0300

    feat: configurar Prisma e NextAuth em TypeScript com provider email

---
+++ src/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,19 @@
+import NextAuth, { NextAuthOptions } from "next-auth";
+import EmailProvider from "next-auth/providers/email";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+export const authOptions: NextAuthOptions = {
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    EmailProvider({
+      server: process.env.EMAIL_SERVER!,
+      from: process.env.EMAIL_FROM!,
+    }),
+  ],
+  secret: process.env.NEXTAUTH_SECRET,
+};
+
+export default NextAuth(authOptions);
